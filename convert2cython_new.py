d5e186f57b89579b45872028e91372c7106ae307
Pham Cong Che <khanhlinhdangthditrach@gmail.com>
2025-03-10 01:16:34 +0300

    add new brand

--- convert2cython_new.py
+++ convert2cython_new.py
@@ -11,6 +11,7 @@ def convert_to_cython(py_file):
                     print(pyd_name)
                     os.remove(pyd_name)
                     return None
+            
             cy_file = os.path.splitext(py_file)[0] + ".pyx"
             with open(cy_file, 'w',encoding="utf-8") as f:
                 f.write(py_code)
@@ -30,41 +31,44 @@ def convert_one(path_:str=r"atklip\graph_objects\chart_component\viewchart.py"):
         os.remove(_cpp_path)
     if os.path.exists(pyx_path):
         os.remove(pyx_path)
-    
-for root, _, files in os.walk("atklip/graphics/chart_component"):
-    for file in files:
-        src_path = os.path.join(root, file)
-        # Bỏ qua thư mục __pycache__
-        if "__pycache__" in src_path:
-            continue
-        
-        elif "__init__" in src_path:
-            continue
-        
-        elif "_ui" in src_path:
-            continue
-        
-        elif "controls" in src_path:
-            continue
-        elif "pyqtgraph" in src_path:
-            continue
-        elif "qfluentwidgets" in src_path:
-            continue
-        elif "app_api" in src_path:
-            continue
-        elif "app_utils" in src_path:
-            continue
-        elif "resource" in src_path:
-            continue
-        elif src_path.endswith(".ui"):
-            continue
-        elif not src_path.endswith(".py"):
-            continue
-        
-        # if src_path.endswith(".pyd"):
-        #     print("delete old",src_path)
-        #     os.remove(src_path)
-        
-        if src_path.endswith(".py"):
-            print(src_path)
-            convert_one(src_path)
+"atklip/graphics/chart_component"
+"atklip/appmanager" "atklip/gui"
+_list_dirs = ["atklip/controls"]
+for dir in _list_dirs:
+    for root, _, files in os.walk(dir):
+        for file in files:
+            src_path = os.path.join(root, file)
+            # Bỏ qua thư mục __pycache__
+            if "__pycache__" in src_path:
+                continue
+            
+            elif "__init__" in src_path:
+                continue
+            
+            elif "_ui" in src_path:
+                continue
+            
+            # elif "controls" in src_path:
+            #     continue
+            elif "pyqtgraph" in src_path:
+                continue
+            elif "qfluentwidgets" in src_path:
+                continue
+            elif "app_api" in src_path:
+                continue
+            elif "app_utils" in src_path:
+                continue
+            elif "resource" in src_path:
+                continue
+            elif src_path.endswith(".ui"):
+                continue
+            elif not src_path.endswith(".py"):
+                continue
+            
+            # if src_path.endswith(".pyd"):
+            #     print("delete old",src_path)
+            #     os.remove(src_path)
+            
+            if src_path.endswith(".py"):
+                print(src_path)
+                convert_one(src_path)
